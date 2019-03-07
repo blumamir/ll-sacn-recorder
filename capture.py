0053beea2bb200bd8472a95aa964a2791b02043f
amir <amirgiraffe@gmail.com>
2019-03-07 20:56:24 +0200

    print messages when config universes do not match network interface universes

--- capture.py
+++ capture.py
@@ -36,6 +36,7 @@ with open(args.config_file) as json_file:
             raise ValueError("num_of_pixels too high for sACN. should be <= 170, recived: " + str(uni_config['num_of_pixels']))
         num_of_channels = uni_config['num_of_pixels'] * channels_per_pixel
         uni_to_range[int(uni)] = (start_index, num_of_channels)
+print("read config file {}, will monitor the following universes: {}".format(args.config_file, list(uni_to_range.keys())))
 
 # open udp recv socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
@@ -43,23 +44,40 @@ sock.bind((args.ip, args.port))
 
 # handle write to file
 f = open(args.out_file, "w+b")
-recv_uni = {}
+recv_uni = set()
 total_frames = 0
+non_manged_uni = set()
 
 while True:
     raw_data, _= sock.recvfrom(1144)  # 1144 because the longest possible packet
     try:
-        tmp_packet = sacn.DataPacket.make_data_packet(raw_data)
+        sacn_packet = sacn.DataPacket.make_data_packet(raw_data)
     except:  # try to make a DataPacket. If it fails just go over it
         continue
 
-    if tmp_packet.universe in uni_to_range:
-        arr_range = uni_to_range[tmp_packet.universe]
-        rgb_data[arr_range[0] : arr_range[0] + arr_range[1]] = bytearray(tmp_packet.dmxData[0 : arr_range[1]])
-        recv_uni[tmp_packet.universe] = True
-        if len(recv_uni) == len(uni_to_range):
-            f.write(rgb_data)
-            total_frames += 1
-            if total_frames % 100 == 0:
-                print('wrote {} frames to file so far'.format(total_frames))
-            recv_uni = {}
+    current_uni = sacn_packet.universe
+
+    if current_uni not in uni_to_range:
+        if current_uni not in non_manged_uni:
+            print("recived a universe '{}' which is not used by the application. ignoring it. this might be a config mistake".format(current_uni))
+            non_manged_uni.add(current_uni)
+        continue
+
+    arr_range = uni_to_range[current_uni]
+    rgb_data[arr_range[0] : arr_range[0] + arr_range[1]] = bytearray(sacn_packet.dmxData[0 : arr_range[1]])
+
+    #try to find config errors (universe in config which is not reported on network)
+    if current_uni in recv_uni:
+        missing_universe = [u for u in uni_to_range.keys() if u not in recv_uni]
+        print("received universe {} while other universes are not found: {}. "
+              "if you see this message too often it means you set these universe in the config file but do not output them to network. "
+              "remove these universe from the config, or make sure they are sent correctly."
+              .format(current_uni, missing_universe))
+    recv_uni.add(current_uni)
+
+    if len(recv_uni) == len(uni_to_range):
+        f.write(rgb_data)
+        total_frames += 1
+        if total_frames % 100 == 0:
+            print('wrote {} frames to file so far'.format(total_frames))
+        recv_uni.clear()
